cc76f73e3ff381dc7eb9ade2d0834fbdc2621d88
Raf-Jlavyan <rafayel.jlavyan@addevice.com>
2024-08-01 15:08:32 +0400

    meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Home Page</title>
+        <meta
+          name="ahrefs-site-verification"
+          content="0b6c66869933ca0ab84ffcf57be696656a84125fd03f6929d34ffeac9282a25d"
+        ></meta>
         <meta name="description" content="This is the home page" />
         <meta property="og:title" content="Home Page" />
         <meta property="og:description" content="This is the home page" />
